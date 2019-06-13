3103e7deb9f9cbbf7a36e7e7d2ad22e6cf9329b7
Tserrien <thegeekest94@gmail.com>
2019-06-14 01:01:19 +0200

    blogpost model built, is added to admin

---
+++ blog/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Post(models.Model):
+
+    #limit input with dropdown
+    STATUS_CHOICES =(
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+
+    #blogpost's title
+    title = models.CharField(max_length = 250)
+
+    #url builder (???)
+    slug = models.SlugField(
+        max_length = 250,
+        #adds publish date to url
+        unique_for_date = 'publish',
+    )
+
+    #foreign key. one user, many posts
+    author = models.ForeignKey(
+        User,
+        on_delete = models.CASCADE,
+        related_name = 'blog_post',
+        )
+
+    #to store the content
+    body = models.TextField()
+
+    #storing the important dates
+    publish = models.DateTimeField(default = timezone.now)
+
+    created = models.DateTimeField(auto_now_add = True)
+
+    #last update only
+    updated = models.DateTimeField(auto_now = True)
+
+    status = models.CharField(
+        max_length = 10,
+        #use dropdown
+        choices = STATUS_CHOICES,
+        default = 'draft'
+    )
+
+    #order posts latest first
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
